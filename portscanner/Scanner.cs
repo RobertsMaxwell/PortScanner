42403fcd95d8eda591d44b17284db69790e405c1
RobertsMaxwell <max031029@gmail.com>
2020-06-09 12:18:15 -0600

    Basic CLI for checking single port

---
+++ portscanner/Scanner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace PortScanner
+{
+    class Scanner
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            Console.Write("Enter an IP Address:");
+            string address = Console.ReadLine();
+            
+            Console.Write("Enter a port to be scanned:");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                client.Connect(address, port);
+                Console.WriteLine($"Port {port} on {address} is open");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Port {port} on {address} is closed");
+            }
+        }
+    }
+}
